22dcf3b3d737e7bbfb3f0e4afa6f2885c7593ea1
Brooke <brookethewitch@gmail.com>
2022-07-15 18:43:16 -0400

    Initial commit

---
+++ app/src/main/java/com/bbonavita/mycontacts/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bbonavita.mycontacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bbonavita.mycontacts.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
